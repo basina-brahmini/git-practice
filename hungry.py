1eea5123da2390f981863d361a3eaa55569c6650
basina-brahmini <43314243+basina-brahmini@users.noreply.github.com>
2018-10-04 15:09:54 +0530

    Update hungry.py

--- hungry.py
+++ hungry.py
@@ -1,7 +1,8 @@
 hungry=input("ARe you Hungry?")
-if hungry=="yes" and hungry=="y":
+if hungry=="yes":
     print("eat samosa")
     print("eat fries")
+    print("stay focused on your work")
 else:
     thirsty=input("Are you thisty?")
     if thirsty=="Yes":
