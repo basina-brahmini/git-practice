a2e615ad69f30e0b0fe4366b841ea91fdf1e6859
basina-brahmini <brahminibasina3993@gmail.com>
2018-10-04 12:56:06 +0530

    adding file to branch

--- hungry.py
+++ hungry.py
@@ -2,5 +2,12 @@ hungry=input("ARe you Hungry?")
 if hungry=="yes":
     print("eat samosa")
 else:
-    print("do your work")
+    thirsty=input("Are you thisty?")
+    if thirsty=="Yes":
+        print("drink water")
+    else:
+        print("stay alive")
+        
+
+        
     
